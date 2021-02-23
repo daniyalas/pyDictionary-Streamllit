d24c40a2715fe2abbf7a45dec45c231c1d9db7eb
Daniyal A. Syed <50423149+daniyalas@users.noreply.github.com>
2021-02-23 23:43:43 +0500

    Add files via upload

--- pyDictionary_Streamlit_app.py
+++ pyDictionary_Streamlit_app.py
@@ -3,15 +3,22 @@ import pandas as pd
 import streamlit as st
 
 from PyDictionary import PyDictionary
-dictionary=PyDictionary()
+dictionary = PyDictionary()
 
-word = ''
 user_input = st.text_input(
     label = "Type your word in the box below",
-    value = word,
+    value = '',
     max_chars = None,
     key = None,
     type = "default")
 
-# word = "indentation"
+st.sidebar.write('Urdu Translation: ' + dictionary.translate(user_input, 'ur'))
+
+'Meanings'
 st.write(dictionary.meaning(user_input))
+
+'Synonyms'
+st.write(dictionary.synonym(user_input))
+
+'Antonyms'
+st.write(dictionary.antonym(user_input))
