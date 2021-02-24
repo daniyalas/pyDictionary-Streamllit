adec212d9dbdb09f3acb478aff2359b9a913e39f
Daniyal A. Syed <50423149+daniyalas@users.noreply.github.com>
2021-02-25 00:36:29 +0500

    Add files via upload

--- pyDictionary_Streamlit_app.py
+++ pyDictionary_Streamlit_app.py
@@ -12,10 +12,11 @@ user_input = st.text_input(
     key = None,
     type = "default")
 
-# Translation is very buggy for some reason
-# st.sidebar.write('### Urdu Translation:')
+# Translation of pyDictionary is buggy so commented out
+# st.sidebar.write('### Spanish Translation:')
 # st.sidebar.write(dictionary.translate(user_input, 'es'))
 
+# Get array of all meanings of the word
 meanings = dictionary.meaning(user_input)
 
 # pos = part of speech
@@ -23,13 +24,14 @@ for pos in meanings.keys():
     st.markdown(f" **_{pos.lower()}_** ") # lowercase, bold, italic
     m = 1
     for meaning in meanings[pos]:
-        st.write(m, '. ' , meaning)
+        st.markdown(f"{m}. {meaning}")
         m += 1
     st.write('') # empty line for white space
         
 # This line creates 2 equal columns
 left_column, right_column = st.beta_columns(2)
 
+# Get array of all synonyms of the word
 synonyms = dictionary.synonym(user_input)
 
 # This 'if' condition ensures the Synonym heading doesn't appear when there's no user_input
@@ -43,6 +45,7 @@ if bool(user_input) == True:
         left_column.markdown(f"{s}. {synonym}")
         s += 1
 
+# Get array of all antonyms of the word
 antonyms = dictionary.antonym(user_input)
 if bool(user_input) == True:
     right_column.write('### Antonyms')
@@ -54,5 +57,5 @@ if bool(user_input) == True:
         right_column.markdown(f"{a}. {antonym}")
         a += 1
 
-st.markdown('---')
+st.markdown('---') # This creates a thin divider
 st.markdown('This streamlit app is developed by [Daniyal A. Syed](https://www.linkedin.com/in/daniyal-as/).')
