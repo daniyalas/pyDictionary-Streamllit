c2ebdc31769326adea29dbe206a3c239624ca2a7
Daniyal A. Syed <50423149+daniyalas@users.noreply.github.com>
2021-02-25 00:09:01 +0500

    Add files via upload

--- pyDictionary_Streamlit_app.py
+++ pyDictionary_Streamlit_app.py
@@ -1,8 +1,6 @@
-import numpy as np
-import pandas as pd
+from PyDictionary import PyDictionary
 import streamlit as st
 
-from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 
 st.title('pyDictionary on Streamlit')
@@ -22,17 +20,19 @@ meanings = dictionary.meaning(user_input)
 
 # pos = part of speech
 for pos in meanings.keys():
-    st.markdown(f"*{pos.lower()}*")
+    st.markdown(f" **_{pos.lower()}_** ") # lowercase, bold, italic
     m = 1
     for meaning in meanings[pos]:
         st.write(m, '. ' , meaning)
         m += 1
-    st.write('')
+    st.write('') # empty line for white space
         
-
+# This line creates 2 equal columns
 left_column, right_column = st.beta_columns(2)
 
 synonyms = dictionary.synonym(user_input)
+
+# This 'if' condition ensures the Synonym heading doesn't appear when there's no user_input
 if bool(user_input) == True:
     left_column.write('### Synonyms')
     
@@ -50,5 +50,9 @@ if bool(user_input) == True:
     # loop through the words
     a = 1
     for antonym in antonyms:
+        # right_column.write(antonym)
         right_column.markdown(f"{a}. {antonym}")
         a += 1
+
+st.markdown('---')
+st.markdown('This streamlit app is developed by [Daniyal A. Syed](https://www.linkedin.com/in/daniyal-as/).')
